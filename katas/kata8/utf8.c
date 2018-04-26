a0c8b8fd255594476ddab73fab0069e5c77c1463
Jesus Ramirez <jesusramirez3194@gmail.com>
2018-04-26 17:45:14 -0500

    kata9

---
+++ katas/kata8/utf8.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+unsigned char *utf8(unsigned char *s)
+{
+  while (*s) {
+    if (*s < 0x80)
+      s++;
+    else if ((s[0] & 0xe0) == 0xc0) {
+      if ((s[1] & 0xc0) != 0x80 ||
+	  (s[0] & 0xfe) == 0xc0)                        
+	return s;
+      else
+	s += 2;
+    } else if ((s[0] & 0xf0) == 0xe0) {
+      if ((s[1] & 0xc0) != 0x80 ||
+	  (s[2] & 0xc0) != 0x80 ||
+	  (s[0] == 0xe0 && (s[1] & 0xe0) == 0x80) ||    
+	  (s[0] == 0xed && (s[1] & 0xe0) == 0xa0) ||    
+	  (s[0] == 0xef && s[1] == 0xbf &&
+	   (s[2] & 0xfe) == 0xbe))                      
+	return s;
+      else
+	s += 3;
+    } else if ((s[0] & 0xf8) == 0xf0) {
+      if ((s[1] & 0xc0) != 0x80 ||
+	  (s[2] & 0xc0) != 0x80 ||
+	  (s[3] & 0xc0) != 0x80 ||
+	  (s[0] == 0xf0 && (s[1] & 0xf0) == 0x80) ||   
+	  (s[0] == 0xf4 && s[1] > 0x8f) || s[0] > 0xf4) 
+	return s;
+      else
+	s += 4;
+    } else
+      return s;
+  }
+
+  return NULL;
+}
